167f2c92897cc957dbe1eb3f625f663f7da19a61
Riczman69 <127401882+Riczman69@users.noreply.github.com>
2023-03-09 18:24:28 +0700

    Add files via upload

---
+++ Makanan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kota;
+
+/**
+ *
+ * @author 62813
+ */
+public class Makanan extends Hidangan {
+    public String disantap(){
+        return this.getNamaHidangan()+ "dimakan";
+                
+     
+    }
+}
---
+++ Minuman.java
@@ -0,0 +1,8 @@
+package kota;
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan()+"diminum";
+        
+    }
+    
+}
