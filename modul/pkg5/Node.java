fb92ae410bf14d4f23b5bfbe1da95cb887f35b8a
Riczman69 <127401882+Riczman69@users.noreply.github.com>
2023-05-25 17:32:26 +0700

    Add files via upload

---
+++ modul/pkg5/Node.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul.pkg5;
+
+/**
+ *
+ * @author 62813
+ */
+public class Node {
+
+     int data;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
